47a350a4c7a3a62469fea4f23ef98714fe5470e2
Nathanael Burt <nathanael.burt@gmail.com>
2014-06-24 07:33:23 -0600

    added script to parse coordinates of all maps in data directory

---
+++ bin/coordinate_parser_script.rb
@@ -0,0 +1,7 @@
+require_relative '../lib/coordinate_parser'
+
+Dir.glob("/Users/nathanaelburt/gSchoolWork/operation-parse-coordinates/data/*.txt") do |file|
+  parsed_file = File.read(file)
+  coordinate_parser = CoordinateParser.new(parsed_file)
+  p coordinate_parser.parse
+end
